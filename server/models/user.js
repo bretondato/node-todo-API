b9d60d107bb0fe914c52aadda4787cd2a23f9001
Brenno Tondato de Faria <brennosmacbookpro@Brennos-MacBook-Pro.local>
2018-04-18 00:11:03 -0300

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -69,7 +69,7 @@ UserSchema.statics.findByToken = function (token) {
     });
 };
 
-UserSchema.pre('save', function (next) {
+UserSchema.pre('save', function (next)  {
     var user = this;
 
     if (user.isModified('password')) {
@@ -84,6 +84,30 @@ UserSchema.pre('save', function (next) {
     }
 
 });
+
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+
+  return User.findOne({email}).then((user) => {
+      if (!user){
+          return Promise.reject();
+      }
+
+      // Aqui foi adicionado mais uma promise pois o mod bcrypt só funciona com callbacks
+      // para retornar o resultado da função compare adicionamos este dentro de uma nova promisse
+
+      return new Promise((resolve, reject) =>{
+          bcrypt.compare(password, user.password, (err, res)=>{
+              if (res){
+                  resolve(user);
+              }else {
+                  reject();
+              }
+          })
+      })
+  })
+
+};
 
 var User = mongoose.model('User', UserSchema);
 
